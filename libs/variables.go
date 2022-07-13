0af56d399cd3de698caa2f0102bbb09574bda23c
sshwy <jy.cat@qq.com>
2022-07-13 21:18:43 +0800

    feat: data source name cli option

--- libs/variables.go
+++ libs/variables.go
@@ -9,12 +9,13 @@ var (
 	BackDomain   string
 	DataDir      string
 	TmpDir       string
-	Sault        string      = "3.1y4a1o5j9"
-	DefaultGroup int         = 1
-	Day          int         = 86400
-	Month        int         = Day * 30
-	Year         int         = Day * 365
-	LangSuf      []string    = []string{".cpp98.cpp", ".cpp11.cpp", ".cpp14.cpp", ".cpp17.cpp", ".cpp20.cpp", ".py2.py", ".py3.py", ".go", ".java", ".c"}
+	Sault        string   = "3.1y4a1o5j9"
+	DefaultGroup int      = 1
+	Day          int      = 86400
+	Month        int      = Day * 30
+	Year         int      = Day * 365
+	LangSuf      []string = []string{".cpp98.cpp", ".cpp11.cpp", ".cpp14.cpp", ".cpp17.cpp", ".cpp20.cpp", ".py2.py", ".py3.py", ".go", ".java", ".c"}
+	DataSource   string
 )
 
 func DirInit() {
